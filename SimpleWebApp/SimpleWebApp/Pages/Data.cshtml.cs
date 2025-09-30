883739a67e1a2075f0c73b0e05bf42816857b609
Henry <doilagi@gmail.com>
2025-09-30 22:13:05 +0200

    update

---
+++ SimpleWebApp/SimpleWebApp/Pages/Data.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleWebApp.Pages
+{
+    public class DataModel : PageModel
+    {
+        public string? FolderCheckResult { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPostCheckFolder()
+        {
+            string path = "/var/webappdata";
+
+            if (Directory.Exists(path))
+            {
+                FolderCheckResult = $"✅ Folder '{path}' exists.";
+            }
+            else
+            {
+                FolderCheckResult = $"❌ Folder '{path}' does not exist.";
+            }
+        }
+    }
+}
